d02c53c4226c7a04f464966d58c567666d19872e
NataliiaMax <105719575+NataliiaMax@users.noreply.github.com>
2023-03-06 21:37:21 +0200

    update json

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,20 +1,20 @@
-import axios from "axios";
+import axios from 'axios';
 
 const contactsInstance = axios.create({
-  baseURL: 'https://63fdf859cd13ced3d7c386d5.mockapi.io/api/n1/contacts',
+  baseURL: 'https://connections-api.herokuapp.com/api/users',
 });
 
-export const fetchContacts = async () => {
-  const { data } = await contactsInstance.get('/');
-  return data;
-};
-
-export const addContact = async data => {
-  const { data: result } = await contactsInstance.post('/', data);
+export const signup = async (data) => {
+  const { data: result } = await contactsInstance.post('/signup', data);
   return result;
 };
 
-export const deleteContact = async id => {
-  const { data } = await contactsInstance.delete(`/${id}`);
-  return data;
+export const addLoginContact = async data => {
+  const { data: result } = await contactsInstance.post('/login', data);
+  return result;
+};
+
+export const logoutContact = async data => {
+  const { data: result } = await contactsInstance.post('/logout', data);
+  return result;
 };
